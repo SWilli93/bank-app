48fb904dca6089f89c2ccd4583f03f6c248f467e
Scott <70402467+SWilli93@users.noreply.github.com>
2024-04-22 22:45:20 -0400

    bank detail component

--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Bank } from '../bank';
 import { BANKS } from '../banks';
 import { NgFor } from '@angular/common';
+import { BankDetailComponent } from '../bank-detail/bank-detail.component';
 
-@Component({
-  selector: 'app-banks',
-  standalone: true,
-  imports: [NgFor],
-  templateUrl: './banks.component.html',
-  styleUrl: './banks.component.css'
+@Component({ 
+ standalone: true, 
+ imports: [NgFor, BankDetailComponent], 
+ selector: 'app-banks', 
+ templateUrl: './banks.component.html', 
+ styleUrls: ['./banks.component.css']
 })
+
 export class BanksComponent implements OnInit { 
   banks = BANKS; 
   selectedBank!: Bank;
